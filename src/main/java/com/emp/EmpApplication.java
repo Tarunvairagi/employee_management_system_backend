fc07c123df969e985179d41c43742e88cecf05f1
Tarunvairagi <tarunbairagi04@gmail.com>
2025-08-26 15:40:59 -0700

    integrate spring profile

--- src/main/java/com/emp/EmpApplication.java
+++ src/main/java/com/emp/EmpApplication.java
@@ -2,14 +2,19 @@ package com.emp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @CrossOrigin(origins = "*")
 public class EmpApplication {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(EmpApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(EmpApplication.class, args);
+		ConfigurableEnvironment environment = context.getEnvironment();
+		System.out.println(environment.getActiveProfiles()[0]);
 	}
 
 }
--- src/main/java/com/emp/config/SecurityConfig.java
+++ src/main/java/com/emp/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -22,6 +23,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
+//@Profile("dev")
 public class SecurityConfig {
     @Autowired
     private final CustomUserDetailsService customUserDetailsService;
